a9c4fe6d52e180db4f930023cdc59438ed291526
Alaabso <abdulaziz.oday.alaabso@gmail.com>
2026-03-03 17:57:54 +0300

    Notes added

---
+++ Week 1/helloagain.cpp
@@ -0,0 +1,14 @@
+//This program demonostrates two new ideas
+//Instead of always writing std::
+//You can import that std namespace with
+//using namespace std
+//Also, we use cin.get() to prevent the output window from closing automatically
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"Hello World again";
+	cin.get();
+	return 0;
+}
